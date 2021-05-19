0ba7368fb8cb9b33719af9d342320dd9d76c19ca
arcanist <arcanistzed@gmail.com>
2021-05-19 16:34:20 -0400

    Template

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+const testModules = require('./test-module');
+require('../css/app.css');
+require('../scss/style.scss');
+
+/********** Paste your code here! ************/
+
+console.log('Paste your code here!');
+console.log(testModules.hello);
