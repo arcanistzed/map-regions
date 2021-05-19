c822e4536858b7c348150d67ae3a273a806aa202
arcanist <arcanistzed@gmail.com>
2021-05-19 16:47:07 -0400

    Fix template

--- webpack.config.js
+++ webpack.config.js
@@ -85,12 +85,6 @@ const CONFIG = {
             },
           },
           {
-            loader: "postcss-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
             loader: "sass-loader",
             options: { sourceMap: true },
           },
