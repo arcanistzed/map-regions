7dad867f71c70ef0127453d397c42928ab413758
arcanist <arcanistzed@gmail.com>
2021-05-19 16:35:04 -0400

    Fix port

--- webpack.config.js
+++ webpack.config.js
@@ -110,7 +110,7 @@ const CONFIG = {
   devServer: {
     contentBase: path.join(__dirname, "src"),
     compress: true,
-    port: 3001,
+    port: 3000,
     hot: true,
     watchContentBase: true,
     noInfo: true,
